4a4b0a5b909ee8edc95574017002d8c2977b435a
MianAnwar <mianmuhammad.anwar@systemsltd.com>
2023-02-16 14:08:40 +0500

    Quater 3 dynamically done

--- src/components/Templates/ProgramsPage/detailPage.tsx
+++ src/components/Templates/ProgramsPage/detailPage.tsx
@@ -7,9 +7,10 @@ import {
   Text,
   chakra, Link,
   Heading,
+  Divider,
 } from '@chakra-ui/react'
-import { GeneralQuaters } from '../../../constants/constants';
-import { ArrowForwardIcon } from '@chakra-ui/icons';
+import { GeneralQuaters, Web3MetaSpecializationQuaters } from '../../../constants/constants';
+import { ArrowForwardIcon, SunIcon } from '@chakra-ui/icons';
 
 export default function ProgramDetailPage({ programTitle }: { programTitle: string }) {
   return (
@@ -56,9 +57,11 @@ export default function ProgramDetailPage({ programTitle }: { programTitle: stri
                           {outline.topicsCovered.length > 0 && <Text as="u" fontSize="medium" fontWeight="medium">Topics Covered:</Text>}
                           {outline.topicsCovered.map((topic, index) => {
                             return <Box key={index} ml="10px">
-                              {topic.link != "" && <Text>{index + 1}. <Link color="blue.600" href={topic.link}>{topic.topic}</Link></Text>}
-                              {topic.link == "" && topic.note == "" && <Text>{index + 1}. {topic.topic}</Text>}
-                              {topic.note != "" && <chakra.text fontSize="small">{topic.note}</chakra.text>}
+                              {index != 0 && topic.note == "project" && <Divider my="10px" />}
+                              {topic.link != "" && <Text><SunIcon /> <Link color="blue.600" href={topic.link}>{topic.topic}</Link></Text>}
+                              {topic.link == "" && topic.note == "" && <Text><SunIcon />.. {topic.topic}</Text>}
+                              {topic.link == "" && topic.note != "" && <Text>{topic.topic}</Text>}
+                              {topic.note != "" && topic.note != "project" && <chakra.text fontSize="small">{topic.note}</chakra.text>}
                             </Box>
                           })}
                         </Box>
@@ -116,6 +119,105 @@ export default function ProgramDetailPage({ programTitle }: { programTitle: stri
         })}
 
         <Heading fontSize="3xl" textAlign="center">{programTitle} Specialization</Heading>
+        {Web3MetaSpecializationQuaters.map(quater => {
+          return <AccordionItem key={quater.quaterNo} mb="60px" mt="30px">
+            <AccordionButton>
+              <Box as="span" flex='1' textAlign='left'>
+                <Text fontSize="20px" fontWeight="bold">{quater.quaterNo} - {quater.quaterName}</Text>
+                <Text>Duration: <chakra.text fontWeight="bold"> {quater.quaterDuration}</chakra.text></Text>
+              </Box>
+              <AccordionIcon />
+            </AccordionButton>
+
+            <AccordionPanel pb={4}>
+              <Text my="10px">{quater.courseDescription}</Text>
+              <Box>
+                <Accordion defaultIndex={[0]} allowMultiple>
+                  <AccordionItem>
+                    <AccordionButton>
+                      <Box as="span" flex='1' textAlign='left'>
+                        <Heading fontSize="3xl" textAlign="center">Outline</Heading>
+                      </Box>
+                      <AccordionIcon />
+                    </AccordionButton>
+                    <AccordionPanel pb={4}>
+                      {quater.courseOutline.map((outline, index) => {
+                        return <Box key={index} my="5px" ml="10px">
+                          <Text fontSize="2xl" fontWeight="bold">{outline.sr}. {outline.mainTitle}</Text>
+
+                          {outline.links.length > 0 && <Text as="u" fontSize="small" fontWeight="medium" >Links:</Text>}
+                          {outline.links.map((link, index) => {
+                            return <Text ml="10px" color="blue.600" key={index}><ArrowForwardIcon /> <Link href={link.link} >{link.title}</Link></Text>
+                          })}
+
+                          {outline?.messages.length > 0 && <Text as="u" fontSize="small" fontWeight="medium">TODOs:</Text>}
+                          {outline.messages.map((message, index) => {
+                            return <Text ml="10px" key={index}>{message}</Text>
+                          })}
+
+                          {outline.topicsCovered.length > 0 && <Text as="u" fontSize="medium" fontWeight="medium">Topics Covered:</Text>}
+                          {outline.topicsCovered.map((topic, index) => {
+                            return <Box key={index} ml="10px">
+                              {index != 0 && topic.note == "project" && <Divider my="10px" />}
+                              {topic.link != "" && <Text><SunIcon /> <Link color="blue.600" href={topic.link}>{topic.topic}</Link></Text>}
+                              {topic.link == "" && topic.note == "" && <Text><SunIcon /> {topic.topic}</Text>}
+                              {topic.link == "" && topic.note != "" && <Text>{topic.topic}</Text>}
+                              {topic.note != "" && topic.note != "project" && <chakra.text fontSize="small">{topic.note}</chakra.text>}
+                            </Box>
+                          })}
+                        </Box>
+                      })}
+
+                    </AccordionPanel>
+                  </AccordionItem>
+
+                  {(quater.quaterBreakTasks.assignments.length > 0 || quater.quaterBreakTasks.quizzes.length > 0) &&
+                    <AccordionItem>
+                      <AccordionButton>
+                        <Box as="span" flex='1' textAlign='left'>
+                          <Heading fontSize="3xl" textAlign="center">{quater.quaterBreakTasks.message}</Heading>
+                        </Box>
+                        <AccordionIcon />
+                      </AccordionButton>
+                      <AccordionPanel pb={4}>
+                        {quater.quaterBreakTasks.studyMaterialLinks.length > 0 && <Text as="u" fontSize="small" fontWeight="medium" >Study Material Link:</Text>}
+                        {quater.quaterBreakTasks.studyMaterialLinks.map((link, index) => {
+                          return <Text ml="10px" color="blue.600" key={index}><Link href={link} >{link}</Link></Text>
+                        })}
+
+                        {quater.quaterBreakTasks.assignments && <Text as="u" fontSize="small" fontWeight="medium" >Assignemts:</Text>}
+                        {quater.quaterBreakTasks.assignments.map((assignment, index) => {
+                          return <Box key={index} ml="10px">
+                            {assignment.link != "" && <Text>{index + 1}. <Link color="blue.600" href={assignment.link}>{assignment.title}</Link></Text>}
+                            {assignment.link == "" && <Text>{index + 1}. {assignment.title}</Text>}
+                            {assignment.subtitle != "" && <chakra.text fontSize="small">{assignment.subtitle}</chakra.text>}
+                          </Box>
+                        })}
+
+
+                        {quater.quaterBreakTasks.quizzes.length > 0 && <Text as="u" fontSize="small" fontWeight="medium" >Quizzes:</Text>}
+                        {quater.quaterBreakTasks.quizzes.map((quiz, index) => {
+                          return <Box key={index} ml="10px" mb="10px">
+                            <Text fontWeight="bold">{quiz.title}</Text>
+                            <Text>Total Questions: {quiz.marking.totalQuestions}</Text>
+                            <Text>Total Time: {quiz.marking.totalTime}</Text>
+                            {quiz.studyLinks.length > 0 && <Text as="u" fontSize="small" fontWeight="medium" >Study Links:</Text>}
+                            {quiz.studyLinks.map((link, index) => {
+                              return <Box key={index} ml="10px">
+                                {link.link != "" && <Text>{index + 1}. <Link color="blue.600" href={link.link}>{link.title}</Link></Text>}
+                                {link.link == "" && <Text>{index + 1}. {link.title}</Text>}
+                              </Box>
+                            })}
+                          </Box>
+                        })}
+                      </AccordionPanel>
+                    </AccordionItem>
+                  }
+                </Accordion>
+              </Box>
+            </AccordionPanel>
+          </AccordionItem>
+        })}
 
       </Accordion>
     </>
--- src/constants/constants.ts
+++ src/constants/constants.ts
@@ -349,7 +349,22 @@ export const GeneralQuaters: Quater[] = [
             title: "API Routes",
             type: "website",
             link: "https://nextjs.org/docs/api-routes/introduction"
-          }
+          },
+          {
+            title: "APIs with Next.js and tRPC",
+            type: "",
+            link: ""
+          },
+          {
+            title: "Build a tRPC CRUD API Example with Next.js",
+            type: "website",
+            link: "https://codevoweb.com/build-a-trpc-crud-api-example-with-next-js/"
+          },
+          {
+            title: "Stop building REST APIs for your Next.js apps, use tRPC instead",
+            type: "website",
+            link: "https://brockherion.dev/blog/posts/stop-building-rest-apis-for-your-next-apps/"
+          },
         ],
         topicsCovered: []
       },
@@ -526,3 +541,302 @@ export const GeneralQuaters: Quater[] = [
     }
   },
 ]
+
+export const Web3MetaSpecializationQuaters: Quater[] = [{
+  quaterNo: "Quarter IV",
+  quaterName: "W3-351: Developing Smart Contracts and Planet-Scale Web 3.0 Dapps",
+  quaterDuration: "13 Weeks",
+  courseDescription: "In this course you will learn how to develop Web 3.0 DApps, create a project, deploy it in production, write smart contracts, unit test them, and create user interfaces for them. We will also learn to develop ERC-20 and NFT tokens, DAOs, Oracles, etc.  Please note that in order to develop Web 3 applications you also need to build on top of Web 2.0 technologies which we have already covered in the previous quarters.",
+  courseOutline: [
+    {
+      sr: "1",
+      mainTitle: "Blockchain and Metaverse Theory",
+      messages: [],
+      links: [
+        {
+          title: "Why You Should Learn Web 2.0 Before Getting into Web 3.0",
+          type: "github",
+          link: "https://javascript.plainenglish.io/why-you-should-learn-web-2-0-before-getting-into-web-3-0-d251625990db"
+        },
+        {
+          title: "The Metaverse: And How It Will Revolutionize Everything by Matthew Ball",
+          type: "website",
+          link: "https://www.amazon.com/Metaverse-How-Will-Revolutionize-Everything/dp/1324092033/ref=tmm_hrd_swatch_0 "
+        },
+        {
+          title: "Mastering Blockchain - Fourth Edition by Imran Bashir",
+          type: "website",
+          link: "https://www.packtpub.com/product/mastering-blockchain-fourth-edition/9781803241067"
+        }
+      ],
+      topicsCovered: []
+    },
+    {
+      sr: "2",
+      mainTitle: "Smart Contract Development in Solidity",
+      messages: [],
+      links: [
+        {
+          title: "Solidity Programming Essentials - Second Edition By Ritesh Modi",
+          type: "website",
+          link: "https://www.packtpub.com/product/solidity-programming-essentials-second-edition/9781803231181"
+        },
+        {
+          title: "Solidity Learning Repo",
+          type: "github",
+          link: "https://github.com/panaverse/defi-dapps-solidity-smart-contracts"
+        }
+      ],
+      topicsCovered: []
+    },
+    {
+      sr: "3",
+      mainTitle: "Dapp Development using Ethers.js and Next.js 13",
+      messages: [],
+      links: [
+        {
+          title: "Dapp Learning Repo",
+          type: "github",
+          link: "https://github.com/panaverse/dapps-nextjs"
+        }
+      ],
+      topicsCovered: []
+    },
+    {
+      sr: "4",
+      mainTitle: "Tokennomics",
+      messages: [],
+      links: [],
+      topicsCovered: []
+    },
+    {
+      sr: "5",
+      mainTitle: "Blockchain Project: Create a Token and Launch ICO/IEO/IDO",
+      messages: [],
+      links: [
+        {
+          title: "As you probably know, the ICO ('Initial Coin Offering') industry has been booming, and it's completely reinventing the way new startups kickstart themselves. In fact, have a look at Wikipedia's list of highest crowdfunding projects and you'll notice that blockchain projects absolutely dominate the list.",
+          type: "website",
+          link: "https://en.wikipedia.org/wiki/List_of_highest-funded_crowdfunding_projects"
+        },
+        {
+          title: "Understand the difference between IDO vs. IEO vs. ICO",
+          type: "website",
+          link: "https://phemex.com/blogs/what-is-a-dex-ido"
+        },
+        {
+          title: "ICO list at ICO Drops",
+          type: "website",
+          link: "https://icodrops.com"
+        },
+        {
+          title: "Top-Rated Crypto Token Sales: List of New ICOs, STOs, IEOs and IDOsTop-Rated Crypto Token Sales: List of New ICOs, STOs, IEOs and IDOs",
+          type: "website",
+          link: "https://cryptototem.com/ico-list/"
+        },
+        {
+          title: "ICO List Online",
+          type: "website",
+          link: "https://www.icolistingonline.com"
+        },
+        {
+          title: "Binance IEO List",
+          type: "website",
+          link: "https://coincodex.com/ieo-list/binance/"
+        },
+        {
+          title: "Binance Launchpad",
+          type: "website",
+          link: "https://www.coinspeaker.com/ieo/platform/binance-launchpad/"
+        },
+        {
+          title: "IEO List",
+          type: "website",
+          link: "https://icomarks.com/ieo"
+        },
+        {
+          title: "Polkastarter",
+          type: "website",
+          link: "https://polkastarter.com"
+        }
+      ],
+      topicsCovered: [
+        {
+          topic: "Project Part 1: How to Launch a IEO on Binance Launchpad",
+          note: "project",
+          link: ""
+        },
+        {
+          topic: "Read How to Launch an IEO",
+          note: "",
+          link: "https://appinventiv.com/blog/how-to-launch-an-ieo/"
+        },
+        {
+          topic: "Your first task of the project is to make a google slides presentation on how to start a IEO on the Binance Launch Pad ",
+          note: "Note: Also document the alternatives to Binance Launchpad.",
+          link: "https://www.binance.com/en/support/faq/94ed108ce89d44ab8602aa3c476dfb04"
+        },
+
+        {
+          topic: "Project Part 2: How to Launch a IDO on Polkastarter",
+          note: "project",
+          link: ""
+        },
+        {
+          topic: "Review the [list of top fundraising platforms]",
+          note: "",
+          link: "https://cryptorank.io/fundraising-platforms"
+        },
+        {
+          topic: "Your second task of the project is to make a google slides presentation on how to start a IDO on the Polkastarter",
+          note: "",
+          link: "https://polkastarter.com/"
+        },
+
+        {
+          topic: "Project Part 3: Create a Pako Token",
+          note: "project",
+          link: ""
+        },
+        {
+          topic: "By creating a token and related contracts, you'll also learn how to handle money sent to your contracts, which should come in handy if you want to create some kind of paid decentralized service in the future.",
+          note: "Therefore, for the sake of this chapter, let's imagine that our  Pako DApp uses its own coin – the Pako Token. We will create two contracts – one for the token itself and one for the token crowd sale (the ICO).",
+          link: ""
+        },
+        {
+          topic: "Now Create your own Pako ERC20 Token and deploy it on a testnet. The Token should be to use OpenZeppelin contracts. You will use the Hardhat development environment. Also, write at least twenty automated tests. We will be using Solidity and Typescript for development.",
+          note: "",
+          link: ""
+        },
+
+        {
+          topic: "Project Part 4: Develop Crowd Sale Contract",
+          note: "project",
+          link: ""
+        },
+        {
+          topic: "This contract will be responsible for allowing users to exchange ETH for our Pako Token. In order to do that we need to set a price for our token (1 ETH = 100 Pako Token)",
+          note: "",
+          link: ""
+        },
+        {
+          topic: "Implement a payable buyToken() function.",
+          note: "Emit a BuyTokens event that will log who’s the buyer, the amount of ETH sent and the amount of Token bought",
+          link: ""
+        },
+        {
+          topic: 'Transfer 75% of the Tokens to the Crowd Sale contract at deployment time. i.e. Right after the contract is deployed, we want the token contract to send 75% of our token supply to it. While 25% remain in our personal "owner" account.',
+          note: "Transfer the ownership of the Crowd Sale contract (at deploy time) to our frontend address so that we are able to withdraw the ETH.",
+          link: ""
+        },
+        {
+          topic: "You can use the openzeppelin crowd sale contracts",
+          note: " however you will have to update the code to the latest solidity version.",
+          link: "https://docs.openzeppelin.com/contracts/4.x/crowdsales"
+        },
+        {
+          topic: "Also write extensive tests, for example we will simply send a transaction of 1 ETH from a random account to the contract. After the transaction, we should expect the account to have received Pako, while the contract's balance should have been reduced.",
+          note: "",
+          link: ""
+        },
+        {
+          topic: "Note: Before you get started writing the token contract we suggest you review the access control",
+          note: "",
+          link: "https://docs.openzeppelin.com/contracts/4.x/access-control"
+        },
+
+        {
+          topic: "Project Part 5: Trying it with MetaMask",
+          note: "project",
+          link: ""
+        },
+        {
+          topic: "While it's always good to test your code, it's often more satisfying to see the results of your work wrapped in a nice UI. Let's see how we can deploy our contracts and get some Pako tokens into our MetaMask wallet!",
+          note: "We start by running our deployment scripts for the test network so that the new token contracts are uploaded and deployed.",
+          link: ""
+        },
+        {
+          topic: "When it's done, take note of what addresses the contracts were uploaded to and copy it!",
+          note: "Now head to MetaMask, and send a transaction of 1 ETH to the crowd sale contract address. If your MetaMask wallet doesn't have any ethers, remember that you can use any faucet.",
+          link: ""
+        },
+        {
+          topic: "After the transaction has been confirmed, you might be confused about why you can't see any tokens in your wallet. It turns out that you need to manually add the token address in MetaMask in order to 'register' it – after all, there are so many tokens out there, there's no way MetaMask could list them all by default!",
+          note: "To do this, open the side menu and click on the 'Add token' button to get started:",
+          link: ""
+        },
+        {
+          topic: "Once you're on the token page, click on 'Add custom token' and paste in the token contract's address in the address field.",
+          note: "After confirming that you want to add the token, you should be able to see your Pako balance right next to your ETH balance in the wallet. How cool!",
+          link: ""
+        },
+
+        {
+          topic: "Project Part 6: Trying it with Multisignature Wallets",
+          note: "project",
+          link: ""
+        },
+        {
+          topic: "Read",
+          note: "Multisignature Wallets",
+          link: "https://www.coindesk.com/tech/2020/11/10/multisignature-wallets-can-keep-your-coins-safer-if-you-use-them-right/"
+        },
+        {
+          topic: "Now use Gnosis Safe",
+          note: "with multi-sigs to do what you did in the last part.",
+          link: "https://gnosis-safe.io/"
+        },
+
+        {
+          topic: "Project Part 7: Sending Tokens using Ethers.js",
+          note: "project",
+          link: ""
+        },
+        {
+          topic: "Write a Typescript program to send Pako Token to some friend's address using Ethers.js.",
+          note: "You may follow above linked tutorial",
+          link: "https://ethereum.org/en/developers/tutorials/send-token-etherjs/"
+        },
+
+        {
+          topic: "Project Part 8 Advance: Create, Deploy, Mint, and Sell an NFT",
+          note: "project",
+          link: ""
+        },
+        {
+          topic: "Read this NFT tutorial series",
+          note: "",
+          link: "https://ethereum.org/en/developers/tutorials/how-to-write-and-deploy-an-nft/"
+        },
+        {
+          topic: "Create a NFT contract using the [OpenZepplen ERC721 NFT Standard]",
+          note: "",
+          link: "https://docs.openzeppelin.com/contracts/4.x/erc721"
+        },
+        {
+          topic: "You may use the Preset ERC721 contract",
+          note: "",
+          link: "https://docs.openzeppelin.com/contracts/4.x/erc721#Presets"
+        },
+        {
+          topic: "Deploy your NFT contract on a testnet, mint it, and view it on the MetaMask wallet and list it on OpenSea Marketplace for sale.",
+          note: "",
+          link: "https://opensea.io/"
+        },
+        {
+          topic: "Implement a ERC721 Market",
+          note: "",
+          link: "https://ethereum.org/en/developers/tutorials/how-to-implement-an-erc721-market/"
+        },
+      ]
+    }
+  ],
+
+  quaterBreakTasks: {
+    message: "Web 2.0 Projects",
+    studyMaterialLinks: [],
+    assignmentHeading: "Project to complete.",
+    assignments: [],
+    quizzes: [],
+  }
+}];
